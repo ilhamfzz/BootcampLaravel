2d54b25595af0d7f5fbe45394ab55d5022e57edd
ilhamfzz <ilhamfz117@gmail.com>
2024-10-09 10:04:46 +0700

    eighth commit: add assignment 12 (Intro Laravel)

---
+++ intro-laravel/resources/views/register.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Sign Up Form</title>
+  </head>
+  <body>
+    <h1>Buat Akun Baru!</h1>
+    <h3>Sign Up Form</h3>
+    <form action="/welcome">
+      <label for="fname">First Name:</label><br />
+      <input type="text" name="fname" id="fname" /><br /><br />
+      <label for="lname">Last Name:</label><br />
+      <input type="text" name="lname" id="lname" /><br /><br />
+      <label for="gender">Gender:</label><br />
+      <input type="radio" name="gender" id="gender" />Male <br />
+      <input type="radio" name="gender" id="gender" />Female <br />
+      <input type="radio" name="gender" id="gender" />Other<br /><br />
+      <label for="nationally">Nationally:</label><br />
+      <select name="nationally" id="nationally">
+        <option value="indonesian">Indonesian</option>
+        <option value="other">Other</option></select
+      ><br /><br />
+      <label for="language">Language Spoken:</label><br />
+      <input type="checkbox" name="language" id="language" />Bahasa Indonesia
+      <br />
+      <input type="checkbox" name="language" id="language" />English <br />
+      <input type="checkbox" name="language" id="language" />Other <br />
+      <br />
+      <label for="bio">Bio:</label> <br />
+      <textarea name="bio" id="bio" rows="10" cols="30"></textarea> <br />
+      <br />
+      <input type="submit" name="submit" />
+    </form>
+  </body>
+</html>
---
+++ intro-laravel/resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Welcome</title>
+  </head>
+  <body>
+    <h1>SELAMAT DATANG!</h1>
+    <h2>
+      Terimakasih telah bergabung di SanberBook. Social media kita bersama!
+    </h2>
+  </body>
+</html>
