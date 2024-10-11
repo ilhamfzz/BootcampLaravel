1e836b0ae4e0b150b5e1f194b679d57c5618abed
ilhamfzz <ilhamfz117@gmail.com>
2024-10-11 20:10:22 +0700

    tenth commit: add assignment 14 (DB Migration Laravel)

---
+++ intro-laravel/database/migrations/2024_10_11_124750_create_books_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('summary');
+            $table->string('image')->nullable();
+            $table->integer('stock');
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+            // the line above is the same as the following line
+            // $table->unsignedBigInteger('category_id');
+            // $table->foreign('category_id')->references('id')->on('categories');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
